7c96c1194e83d0c50ee34a57fb12994ccb8e88df
Антон <antonolegovich2017@icloud.com>
2022-11-02 23:48:34 +0600

    dz

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import static MultiplicationTable.MultiplicationTable.generatePairNumber;
+import static MultiplicationTable.MultiplicationTable.getSetTasks;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int count=0;
+        while (count<15){
+            String pairNumber=generatePairNumber();
+            String inverseNumber= pairNumber.charAt(2)+"*"+pairNumber.charAt(0);
+            if (!getSetTasks().contains(pairNumber)&& !getSetTasks().contains(inverseNumber)){
+                getSetTasks().add(pairNumber);
+                count++;
+            }
+        }
+        System.out.println(getSetTasks());
+
+
     }
 }
